6f16a4a53be8d7627e56571e9836dc24580bbb99
jorhyv <jorhyv@gmail.com>
2014-10-14 13:05:56 +0200

    First commit for Create data model #1

---
+++ Coc.Entities/Owner.cs
@@ -0,0 +1,8 @@
+﻿namespace Coc.Entities
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Coc.Entities/PublicPoint.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Spatial;
+
+namespace Coc.Entities
+{
+    public class PublicPoint
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DbGeography Position { get; set; }
+        public Owner Owner { get; set; }
+    }
+}
